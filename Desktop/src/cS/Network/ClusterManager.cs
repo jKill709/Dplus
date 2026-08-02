ad8a70549426ddd15489564035587104ad22891a
jKill709 <jeremykillinger@gmail.com>
2026-08-02 14:22:05 -0400

    Moved ClusterStatus to separate file

--- Desktop/src/cS/Network/ClusterManager.cs
+++ Desktop/src/cS/Network/ClusterManager.cs
@@ -865,29 +865,4 @@ namespace Dplus_Desktop
         }
     }
 
-    public sealed class ClusterStatus
-    {
-        public bool SSHConnected { get; init; }
-
-        public int NodeCount { get; init; }
-
-        public ServiceStatus HubServiceStatus { get; init; }
-        public Dictionary<string, ServiceStatus> NodeServiceStatuses { get; init; }
-
-        public ClusterStatus(bool SSHConnected, int NodeCount, ServiceStatus HubServiceStatus, Dictionary<string, ServiceStatus> NodeServiceStatuses)
-        {
-            this.SSHConnected = SSHConnected;
-            this.NodeCount = NodeCount;
-            this.HubServiceStatus = HubServiceStatus;
-            this.NodeServiceStatuses = NodeServiceStatuses;
-        }
-        public ClusterStatus()
-        {
-            this.SSHConnected = false;
-            this.NodeCount = 0;
-            this.HubServiceStatus = ServiceStatus.Error;
-            this.NodeServiceStatuses = new Dictionary<string, ServiceStatus>();
-        }
-    }
-
 }
---
+++ Desktop/src/cS/Network/ClusterStatus.cs
@@ -0,0 +1,35 @@
+﻿using jCommunicator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dplus_Desktop
+{
+
+    public sealed class ClusterStatus
+    {
+        public bool SSHConnected { get; init; }
+
+        public int NodeCount { get; init; }
+
+        public ServiceStatus HubServiceStatus { get; init; }
+        public Dictionary<string, ServiceStatus> NodeServiceStatuses { get; init; }
+
+        public ClusterStatus(bool SSHConnected, int NodeCount, ServiceStatus HubServiceStatus, Dictionary<string, ServiceStatus> NodeServiceStatuses)
+        {
+            this.SSHConnected = SSHConnected;
+            this.NodeCount = NodeCount;
+            this.HubServiceStatus = HubServiceStatus;
+            this.NodeServiceStatuses = NodeServiceStatuses;
+        }
+        public ClusterStatus()
+        {
+            this.SSHConnected = false;
+            this.NodeCount = 0;
+            this.HubServiceStatus = ServiceStatus.Error;
+            this.NodeServiceStatuses = new Dictionary<string, ServiceStatus>();
+        }
+    }
+}
